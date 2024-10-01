6d39d8cf79aac4792bce2625410fa2ba33b99034
Fabrii01 <fabrizioleturia5@gmail.com>
2024-10-01 14:16:26 -0500

    git add myProgram.cs

--- Moanso/Program.cs
+++ Moanso/Program.cs
@@ -1,1 +1,38 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Solicitar peso y altura
+        Console.Write("Ingrese su peso en kilogramos: ");
+        double peso = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese su altura en metros: ");
+        double altura = Convert.ToDouble(Console.ReadLine());
+
+        // Calcular el IMC
+        double imc = peso / (altura * altura);
+
+        // Mostrar el resultado del IMC
+        Console.WriteLine($"\nSu IMC es: {imc:F2}");
+
+        // Clasificar el IMC según las categorías estándar
+        if (imc < 18.5)
+        {
+            Console.WriteLine("Clasificación: Bajo peso");
+        }
+        else if (imc >= 18.5 && imc < 24.9)
+        {
+            Console.WriteLine("Clasificación: Peso normal");
+        }
+        else if (imc >= 25 && imc < 29.9)
+        {
+            Console.WriteLine("Clasificación: Sobrepeso");
+        }
+        else
+        {
+            Console.WriteLine("Clasificación: Obesidad");
+        }
+    }
+}
